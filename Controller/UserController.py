d4a1eb1794cc19dcab5004b22e1dbcd99561ad75
Stefanos Sophocleous <stefanos.sophocleous@drgok.com>
2023-05-15 20:36:25 -0700

    Everything but Update is done in User Endpoint.. Finishing Now

--- Controller/UserController.py
+++ Controller/UserController.py
@@ -5,29 +5,10 @@ from Models.BlogModel import Blog
 from flask_jwt import jwt_required
 import re
 from Services.UserService import UserService
-
 user_blueprint = Blueprint('users', __name__)
 
-#****************************
-
-#***** GET USERS BY ID  ***** 
-# *****  CREATE AN ACCOUNT  ***** 
-# ***** RETURN ALL USERS ***** 
-# ***** DELETE USER BY ID *****
-
-#*************************************
-
-
-#***** REGEX **********
-
-###^ indicates the start of the string.
-#\d represents any digit character.
-#+ specifies that the previous element (digit) can occur one or more times.
-#$ denotes the end of the string.
-#*********************************************
-
 @user_blueprint.route('/user/<int:id>', methods=['GET'])
-def get_user(id):
+def get_userById(id):
     return UserService.get_userById(id)
 
 @user_blueprint.route('/user/create/<string:email>/<string:username>/<string:password>', methods=['POST'])
@@ -36,36 +17,14 @@ def register_user_account(email, username, password, blog_id = None):
 
 @user_blueprint.route('/users', methods=['GET'])
 def return_all_users():
-    users = User.query.all()
-    if users:
-        user_list = [user.json() for user in users]
-        return jsonify(user_list)
-    else:
-        return jsonify({'name': None}), 404
-
+    return UserService.return_all_users()
 
 @user_blueprint.route('/user/delete/<int:id>', methods=['DELETE'])
 def delete_user_by_Id(id):
-    user = User.query.filter_by(id=id).first()
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({'note': 'delete success'})
-    else:
-        return jsonify({'note': 'user not found'}), 404
+    return UserService.delete_user_by_Id(id)
 
 @user_blueprint.route('/user/update/<int:id>/<string:email>/<string:username>/<string:password>', methods=['PUT'])
 def update_user_information(id, email, username, password):
-    user = User.query.get(id)
-    if user:
-        user.email = email
-        user.username = username
-        user.password_hash = password
-        db.session.commit()
-        return jsonify({'message': 'User information updated successfully'})
-    else:
-        return jsonify({'message': 'User not found'})
-
-    #email=email, username=username, password=password
-
+    return UserService.update_user_information(id, email, username, password)
+   
 
--- Services/UserService.py
+++ Services/UserService.py
@@ -3,7 +3,7 @@ import re
 from Models.UserModel import User
 from flask import jsonify, abort
 from Database.database import db
-
+from sqlalchemy.orm.exc import NoResultFound
 class UserService: 
     def get_userById(id):
         try:
@@ -18,12 +18,11 @@ class UserService:
         except (TypeError) as error:
             return jsonify(str(error))
     
-    
     def register_user_account(email, username, password, blog_id=None):
         try:
-            is_valid_email(email)
-            is_valid_username(username)
-            is_valid_password(password)
+            UserService.is_valid_email(email)
+            UserService.is_valid_username(username)
+            UserService.is_valid_password(password)
 
             if blog_id is not None: 
                 user = User(email=email, username=username, password=password, blog_id=blog_id)
@@ -35,34 +34,73 @@ class UserService:
         except ValueError as error:
             abort(400, str(error))
 
-def is_valid_email(email):
-    if not validate_email(email):
-        raise ValueError('Invalid Email. This does not exist.')
+    def is_valid_email(email):
+        if not validate_email(email):
+            raise ValueError('Invalid Email. This does not exist.')
 
-def is_valid_username(username):
-    if len(username) > 12:
-        raise ValueError('Invalid Username. Maximum length exceeded.')
+    def is_valid_username(username):
+        if len(username) > 12:
+            raise ValueError('Invalid Username. Maximum length exceeded.')
 
-def is_valid_password(password):
+    def is_valid_password(password):
     # Check minimum length of 8 characters
-    if len(password) < 8:
-        raise ValueError('Invalid Password. Minimum length is 8 characters.')
+        if len(password) < 8:
+            raise ValueError('Invalid Password. Minimum length is 8 characters.')
     
     # Check for at least one uppercase letter
-    if not re.search(r'[A-Z]', password):
-        raise ValueError('Invalid Password. At least one uppercase letter is required.')
+        if not re.search(r'[A-Z]', password):
+            raise ValueError('Invalid Password. At least one uppercase letter is required.')
     
     # Check for at least one lowercase letter
-    if not re.search(r'[a-z]', password):
-        raise ValueError('Invalid Password. At least one lowercase letter is required.')
+        if not re.search(r'[a-z]', password):
+            raise ValueError('Invalid Password. At least one lowercase letter is required.')
     
     # Check for at least one digit
-    if not re.search(r'\d', password):
-        raise ValueError('Invalid Password. At least one digit is required.')
+        if not re.search(r'\d', password):
+            raise ValueError('Invalid Password. At least one digit is required.')
     
     # Check for at least one special character
-    if not re.search(r'[^a-zA-Z0-9]', password):
-        raise ValueError('Invalid Password. At least one special character is required.')
+        if not re.search(r'[^a-zA-Z0-9]', password):
+            raise ValueError('Invalid Password. At least one special character is required.')
     
     # All conditions met, password is strong
-    return True
+        return True
+
+    def return_all_users():
+
+        try:
+            users = User.query.all()
+            if users:
+                user_list = [user.json() for user in users]
+                return jsonify(user_list)
+            else:
+                raise NoResultFound('You attempted to query an empty database of users')
+        except NoResultFound as error:
+            abort(400, str(error))
+    def delete_user_by_Id(id):
+        try:
+            user = User.query.filter_by(id=id).first()
+            if user:
+                db.session.delete(user)
+                db.session.commit()
+                return jsonify({'note': 'delete success'})
+            else:
+                raise NoResultFound('Attempted to delete a non existant id')
+        except NoResultFound as error:
+            abort(400, str(error))
+    
+    def update_user_information(id, email, username, password):
+        try:
+            user = User.query.get(id)
+            if user:
+                user.email = UserService.is_valid_email(email)
+                user.username = UserService.is_valid_username(username)
+                user.password_hash = UserService.is_valid_password(password)
+                db.session.commit()
+                return jsonify({'message': 'User information updated successfully'})
+            else:
+                raise NoResultFound('Attempted to update a non existent user')
+        except NoResultFound as error:
+            abort(400, str(error))
+    
+    
